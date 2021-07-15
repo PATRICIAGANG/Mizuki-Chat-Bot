4afd6e00d38edc9ba3876fe8dbdff085dc6f6a53
PATRICIAGANG <85884951+PATRICIAGANG@users.noreply.github.com>
2021-07-15 13:21:35 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,4 +2,4 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "1812231298:AAEsS5J4ViP1KSiSW4sitB6OB_mMHLV8iKE")
